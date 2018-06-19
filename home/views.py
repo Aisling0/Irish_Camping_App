d72bad40a59aa4c9cb3a5febaf22a750d97c5d76
“Aisling <aisling1odonovan@gmail.comgit commit -m Initial>
2018-06-19 21:15:17 +0100

    about

--- home/views.py
+++ home/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 def get_index(request):
 
-    return render(request, 'templates/index.html')
+    return render(request, 'index.html')
+
 
 def about(request):
     return render(request, "about.html")
