144d4372e6e8d8c54e3220cc95132e4e20152a27
“Aisling <aisling1odonovan@gmail.comgit commit -m Initial>
2018-06-20 21:46:26 +0100

    carousel_img

--- settings/base.py
+++ settings/base.py
@@ -23,9 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^7p39$sjbz$sv$5_45a%bh4l6*#_ktwxatk4#9bq1del37!&0u'
 
 
-ALLOWED_HOSTS = ['127.0.0.1', 'irish-camping.herokuapp.com']
+ALLOWED_HOSTS = ['irish-camping.herokuapp.com', '127.0.0.1']
 SITE_ID = 3
-INTERNAL_IPS = ('127.0.0.1',)
+
+# INTERNAL_IPS = ('127.0.0.1',)
 
 # Application definition
 
